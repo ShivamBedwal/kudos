967d1f89e2711ecad3cedbb1bf20a85a8fed7d9a
Shivam Bedwal <shivambedwal@gmail.com>
2024-06-28 16:16:51 +0000

    Top kudoed UI changes

--- app/kudos.ui/webapp/controller/MyTeam.controller.js
+++ app/kudos.ui/webapp/controller/MyTeam.controller.js
@@ -8,6 +8,21 @@ sap.ui.define(
         return BaseController.extend("kudos.ui.controller.MyTeam", {
 
             onInit: function () { },
+
+            async addTeamMember(oEvent) {
+
+                var oInput = await this.loadFragment({
+                    name: "kudos.ui.view.NewTeamMember"
+                });
+
+                var oItem = new sap.m.ColumnListItem({
+                    cells: [oInput]
+                });
+
+                var oMyTeamTable = this.byId("idMyTeamTable");
+                oMyTeamTable.addItem(oItem);
+            }
+
         });
     }
 )
